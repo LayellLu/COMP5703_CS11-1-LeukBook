2954584a6cdbb81bddb4ade1c78b3190be2affb3
LayellLu <lu.wesley156@gmail.com>
2022-11-06 20:57:30 +1100

    Project initialization #11

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -17,11 +17,6 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i>Cart
-                </Nav.Link>
-              </LinkContainer>
 
                 <LinkContainer to="/register">
                   <Nav.Link>
